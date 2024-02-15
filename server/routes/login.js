eb126fd4a0584eb5b3767d24feed8c51d2faae57
tedy97123 <tedyyohanes97@gmail.com>
2024-02-15 02:39:01 -0800

    testing

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,17 +6,17 @@ const router = express.Router();
 router.post("/Login", async (req, res) => {
    const { email, password } = req.body; 
   try {
-    // search db for user with id from req from client
     const user = await User.find({'email':email});
     if (!user) {
       return res.status(404).json({ message: "User not found" });
-    }
-    // truthys to validate 
+    } else {
+       // truthys to validate 
       if (email) user.email = email;
       if (password) {
         user.password = password;
       }
      res.status(200).json({message:"User Logged In!"});
+    } 
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
