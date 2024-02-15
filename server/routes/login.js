a59c6038796150d9b937e47ec498f6065339fc95
tedy97123 <tedyyohanes97@gmail.com>
2024-02-15 03:07:03 -0800

    testing

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,12 +7,10 @@ router.post("/Login", async (req, res) => {
    const { email, password } = req.body; 
   try {
     const user = await User.find({'email':email  , 'password':password});
-    console.log(user)
     if ( user == []  ) {
       return res.status(404).json({ message: "User not found" });
-    } else {
-      res.status(200).json({message: "User Logged In"})
-    } 
+    }  
+     res.status(200).json({message: "User Logged In"}) 
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
