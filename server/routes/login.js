6d06728b943e8549fc6f3173829e8a3fb27377be
tedy97123 <tedyyohanes97@gmail.com>
2024-02-15 03:31:11 -0800

    testing mongoose string

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/Login", async (req, res) => {
    const { email, password } = req.body; 
   try {
-    let user = await User.find({'email':email  , 'password':password}) ?  res.status(404).json({ message: "User not found" })  :  res.status(200).json({message: "User Logged In"});
+    let user = await User.find({'email':email  , 'password':password}) ?    res.status(200).json({message: "User Logged In"}) :res.status(404).json({ message: "User not found" }) ;
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
